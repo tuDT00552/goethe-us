2026886068e5a21585b9a3efaaa09a39c42453c5
Your Name <you@example.com>
2023-11-09 23:20:51 +0700

    up

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const mysql = require('mysql2');
-const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path');
 const app = express()
 
 app.use(cors());
@@ -10,111 +8,111 @@ app.use(cors());
 app.use(express.json());
 
 const dbConfig = {
-    host: '103.200.23.80',
-    user: 'herokuap_tudt',
-    password: 'Agglbtpg123',
-    database: 'herokuap_tudt',
-    waitForConnections: true,
-    connectionLimit: 10000,
-    queueLimit: 0,
-    connectTimeout: 180000
+  host: '103.200.23.80',
+  user: 'herokuap_tudt',
+  password: 'Agglbtpg123',
+  database: 'herokuap_tudt',
+  waitForConnections: true,
+  connectionLimit: 10000,
+  queueLimit: 0,
+  connectTimeout: 180000
 };
 
 const pool = mysql.createPool(dbConfig);
 
 async function queryWithRetry(query, params) {
-    try {
-        const [results] = await pool.promise().query(query, params);
-        return results;
-    } catch (error) {
-        if (error.code === 'ETIMEDOUT') {
-            return queryWithRetry(query, params);
-        } else {
-            throw error;
-        }
+  try {
+    const [results] = await pool.promise().query(query, params);
+    return results;
+  } catch (error) {
+    if (error.code === 'ETIMEDOUT') {
+      return queryWithRetry(query, params);
+    } else {
+      throw error;
     }
+  }
 }
 
 app.get('/api/users', async (req, res) => {
-    try {
-        const { isActive } = req.query || 1;
-        const results = await queryWithRetry(
-            'SELECT * FROM users WHERE isActive = ? ORDER BY startDate ASC, endDate ASC',
-            [isActive]
-        );
+  try {
+    const { isActive } = req.query || 1;
+    const results = await queryWithRetry(
+      'SELECT * FROM users WHERE isActive = ? ORDER BY startDate ASC, endDate ASC',
+      [isActive]
+    );
 
-        const processedResults = results.map(row => ({
-            ...row,
-            isActive: row.isActive === 1 ? true : false,
-        }));
+    const processedResults = results.map(row => ({
+      ...row,
+      isActive: row.isActive === 1 ? true : false,
+    }));
 
-        res.json(processedResults);
-    } catch (error) {
-        console.error('Lỗi truy vấn: ' + error.stack);
-        res.status(500).json({ error: 'Lỗi truy vấn cơ sở dữ liệu' });
-    }
+    res.json(processedResults);
+  } catch (error) {
+    console.error('Lỗi truy vấn: ' + error.stack);
+    res.status(500).json({ error: 'Lỗi truy vấn cơ sở dữ liệu' });
+  }
 });
 
 
 app.get('/api/users/update', async (req, res) => {
-    const { id, isActive } = req.query;
+  const { id, isActive } = req.query;
 
-    if (!id || !isActive || (isActive !== '0' && isActive !== '1')) {
-        return res.status(400).json({ error: 'Thiếu tham số hoặc tham số không hợp lệ' });
-    }
+  if (!id || !isActive || (isActive !== '0' && isActive !== '1')) {
+    return res.status(400).json({ error: 'Thiếu tham số hoặc tham số không hợp lệ' });
+  }
 
-    try {
-        const connection = mysql.createConnection(dbConfig);
+  try {
+    const connection = mysql.createConnection(dbConfig);
 
-        connection.query('UPDATE users SET isActive = ? WHERE id = ?', [isActive, id], (error, results) => {
-            connection.end();
+    connection.query('UPDATE users SET isActive = ? WHERE id = ?', [isActive, id], (error, results) => {
+      connection.end();
 
-            if (error) {
-                console.error('Lỗi cập nhật: ' + error.stack);
-                res.status(500).json({ error: 'Lỗi cập nhật cơ sở dữ liệu' });
-            } else {
-                if (results.affectedRows === 1) {
-                    res.json({ success: 'Cập nhật thành công' });
-                } else {
-                    res.status(404).json({ error: 'Không tìm thấy người dùng với id đã cho' });
-                }
-            }
-        });
-    } catch (error) {
-        console.error('Lỗi kết nối: ' + error.stack);
-        res.status(500).json({ error: 'Lỗi kết nối cơ sở dữ liệu' });
-    }
+      if (error) {
+        console.error('Lỗi cập nhật: ' + error.stack);
+        res.status(500).json({ error: 'Lỗi cập nhật cơ sở dữ liệu' });
+      } else {
+        if (results.affectedRows === 1) {
+          res.json({ success: 'Cập nhật thành công' });
+        } else {
+          res.status(404).json({ error: 'Không tìm thấy người dùng với id đã cho' });
+        }
+      }
+    });
+  } catch (error) {
+    console.error('Lỗi kết nối: ' + error.stack);
+    res.status(500).json({ error: 'Lỗi kết nối cơ sở dữ liệu' });
+  }
 });
 
 
 app.get('/api/users/delete', async (req, res) => {
-    const { id } = req.query;
+  const { id } = req.query;
 
-    if (!id) {
-        return res.status(400).json({ error: 'Thiếu tham số id' });
-    }
+  if (!id) {
+    return res.status(400).json({ error: 'Thiếu tham số id' });
+  }
 
-    try {
-        const connection = mysql.createConnection(dbConfig);
+  try {
+    const connection = mysql.createConnection(dbConfig);
 
-        connection.query('DELETE FROM users WHERE id = ?', [id], (error, results) => {
-            connection.end();
+    connection.query('DELETE FROM users WHERE id = ?', [id], (error, results) => {
+      connection.end();
 
-            if (error) {
-                console.error('Lỗi xoá: ' + error.stack);
-                res.status(500).json({ error: 'Lỗi xoá cơ sở dữ liệu' });
-            } else {
-                if (results.affectedRows === 1) {
-                    res.json({ success: 'Xoá thành công' });
-                } else {
-                    res.status(404).json({ error: 'Không tìm thấy người dùng với id đã cho' });
-                }
-            }
-        });
-    } catch (error) {
-        console.error('Lỗi kết nối: ' + error.stack);
-        res.status(500).json({ error: 'Lỗi kết nối cơ sở dữ liệu' });
-    }
+      if (error) {
+        console.error('Lỗi xoá: ' + error.stack);
+        res.status(500).json({ error: 'Lỗi xoá cơ sở dữ liệu' });
+      } else {
+        if (results.affectedRows === 1) {
+          res.json({ success: 'Xoá thành công' });
+        } else {
+          res.status(404).json({ error: 'Không tìm thấy người dùng với id đã cho' });
+        }
+      }
+    });
+  } catch (error) {
+    console.error('Lỗi kết nối: ' + error.stack);
+    res.status(500).json({ error: 'Lỗi kết nối cơ sở dữ liệu' });
+  }
 });
 
 
@@ -122,103 +120,123 @@ app.get('/api/users/delete', async (req, res) => {
 
 
 app.get('/api/users-reg', async (req, res) => {
-    try {
-      const bot = req.query.bot; // Lấy giá trị của tham số bot từ request query.
-      const isActive = req.query.isActive || 1;
-  
-      let query;
-      let queryParams;
-  
-      if (bot) {
-        // Nếu có tham số bot, sử dụng LIKE để tìm kiếm trong cột bot.
-        query = 'SELECT * FROM users1 WHERE isActive = ? AND bot LIKE ? ORDER BY startDate ASC, endDate ASC';
-        queryParams = [isActive, `%${bot}%`];
+  try {
+    const bot = req.query.bot; // Lấy giá trị của tham số bot từ request query.
+    const isActive = req.query.isActive || 1;
+
+    let query;
+    let queryParams;
+
+    if (bot) {
+      // Nếu có tham số bot, sử dụng LIKE để tìm kiếm trong cột bot.
+      query = 'SELECT * FROM users1 WHERE isActive = ? AND bot LIKE ? ORDER BY startDate ASC, endDate ASC';
+      queryParams = [isActive, `%${bot}%`];
+    } else {
+      // Nếu không có tham số bot, truy vấn tất cả dữ liệu.
+      query = 'SELECT * FROM users1 WHERE isActive = ? ORDER BY startDate ASC, endDate ASC';
+      queryParams = [isActive];
+    }
+
+    const results = await queryWithRetry(query, queryParams);
+
+    const processedResults = results.map(row => ({
+      ...row,
+      isActive: row.isActive === 1 ? true : false,
+    }));
+
+    res.json(processedResults);
+  } catch (error) {
+    console.error('Lỗi truy vấn: ' + error.stack);
+    res.status(500).json({ error: 'Lỗi truy vấn cơ sở dữ liệu' });
+  }
+});
+
+
+
+app.get('/api/users-reg/update', async (req, res) => {
+  const { id, isActive } = req.query;
+
+  if (!id || !isActive || (isActive !== '0' && isActive !== '1')) {
+    return res.status(400).json({ error: 'Thiếu tham số hoặc tham số không hợp lệ' });
+  }
+
+  try {
+    const connection = mysql.createConnection(dbConfig);
+
+    connection.query('UPDATE users1 SET isActive = ? WHERE id = ?', [isActive, id], (error, results) => {
+      connection.end();
+
+      if (error) {
+        console.error('Lỗi cập nhật: ' + error.stack);
+        res.status(500).json({ error: 'Lỗi cập nhật cơ sở dữ liệu' });
       } else {
-        // Nếu không có tham số bot, truy vấn tất cả dữ liệu.
-        query = 'SELECT * FROM users1 WHERE isActive = ? ORDER BY startDate ASC, endDate ASC';
-        queryParams = [isActive];
+        if (results.affectedRows === 1) {
+          res.json({ success: 'Cập nhật thành công' });
+        } else {
+          res.status(404).json({ error: 'Không tìm thấy người dùng với id đã cho' });
+        }
       }
-  
-      const results = await queryWithRetry(query, queryParams);
-  
-      const processedResults = results.map(row => ({
-        ...row,
-        isActive: row.isActive === 1 ? true : false,
-      }));
-  
-      res.json(processedResults);
-    } catch (error) {
-      console.error('Lỗi truy vấn: ' + error.stack);
-      res.status(500).json({ error: 'Lỗi truy vấn cơ sở dữ liệu' });
-    }
-  });
-  
-  
-  
-  app.get('/api/users-reg/update', async (req, res) => {
-    const { id, isActive } = req.query;
-  
-    if (!id || !isActive || (isActive !== '0' && isActive !== '1')) {
-      return res.status(400).json({ error: 'Thiếu tham số hoặc tham số không hợp lệ' });
-    }
-  
-    try {
-      const connection = mysql.createConnection(dbConfig);
-  
-      connection.query('UPDATE users1 SET isActive = ? WHERE id = ?', [isActive, id], (error, results) => {
-        connection.end();
-  
-        if (error) {
-          console.error('Lỗi cập nhật: ' + error.stack);
-          res.status(500).json({ error: 'Lỗi cập nhật cơ sở dữ liệu' });
+    });
+  } catch (error) {
+    console.error('Lỗi kết nối: ' + error.stack);
+    res.status(500).json({ error: 'Lỗi kết nối cơ sở dữ liệu' });
+  }
+});
+
+
+app.get('/api/users-reg/delete', async (req, res) => {
+  const { id } = req.query;
+
+  if (!id) {
+    return res.status(400).json({ error: 'Thiếu tham số id' });
+  }
+
+  try {
+    const connection = mysql.createConnection(dbConfig);
+
+    connection.query('DELETE FROM users1 WHERE id = ?', [id], (error, results) => {
+      connection.end();
+
+      if (error) {
+        console.error('Lỗi xoá: ' + error.stack);
+        res.status(500).json({ error: 'Lỗi xoá cơ sở dữ liệu' });
+      } else {
+        if (results.affectedRows === 1) {
+          res.json({ success: 'Xoá thành công' });
         } else {
-          if (results.affectedRows === 1) {
-            res.json({ success: 'Cập nhật thành công' });
-          } else {
-            res.status(404).json({ error: 'Không tìm thấy người dùng với id đã cho' });
-          }
+          res.status(404).json({ error: 'Không tìm thấy người dùng với id đã cho' });
         }
-      });
-    } catch (error) {
-      console.error('Lỗi kết nối: ' + error.stack);
-      res.status(500).json({ error: 'Lỗi kết nối cơ sở dữ liệu' });
+      }
+    });
+  } catch (error) {
+    console.error('Lỗi kết nối: ' + error.stack);
+    res.status(500).json({ error: 'Lỗi kết nối cơ sở dữ liệu' });
+  }
+});
+
+app.get('/api/get-url', async (req, res) => {
+  try {
+    const query = 'SELECT * FROM urls LIMIT 1';
+
+    const results = await queryWithRetry(query);
+
+    if (results.length > 0) {
+      const url = results[0].url;
+      res.json(url);
+    } else {
+      res.status(404).json({ error: 'Không tìm thấy bản ghi nào trong bảng "urls"' });
     }
-  });
-  
-  
-  app.get('/api/users-reg/delete', async (req, res) => {
-    const { id } = req.query;
-  
-    if (!id) {
-      return res.status(400).json({ error: 'Thiếu tham số id' });
-    }
-  
-    try {
-      const connection = mysql.createConnection(dbConfig);
-  
-      connection.query('DELETE FROM users1 WHERE id = ?', [id], (error, results) => {
-        connection.end();
-  
-        if (error) {
-          console.error('Lỗi xoá: ' + error.stack);
-          res.status(500).json({ error: 'Lỗi xoá cơ sở dữ liệu' });
-        } else {
-          if (results.affectedRows === 1) {
-            res.json({ success: 'Xoá thành công' });
-          } else {
-            res.status(404).json({ error: 'Không tìm thấy người dùng với id đã cho' });
-          }
-        }
-      });
-    } catch (error) {
-      console.error('Lỗi kết nối: ' + error.stack);
-      res.status(500).json({ error: 'Lỗi kết nối cơ sở dữ liệu' });
-    }
-  });
+  } catch (error) {
+    console.error('Lỗi truy vấn: ' + error.stack);
+    res.status(500).json({ error: 'Lỗi truy vấn cơ sở dữ liệu' });
+  }
+});
+
+
 
 
 app.listen(3000, () => {
-    console.log('Server is up on 3000')
+  console.log('Server is up on 3000')
 })
 
 
